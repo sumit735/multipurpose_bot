221867db3bfd4fcaf78b15c0bbcf3494cd8c1820
sumit735 <sumitkumardash1999@gmail.com>
2020-07-08 19:11:16 +0530

    retry new line

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ bot.command('covid19', async (ctx) => {
     const data = await getData();
     console.log(data);
     // ctx.reply('we are fetching covid details');
-    ctx.reply(`Country- IndiaTotal Cases- ${data.active} /n 😵Confirmed Cases- ${data.confirmed} 😢 /n  Deaths- ${data.deaths} 😭 /n New Cases Today- ${data.deltaconfirmed} 😱 /n New Recovered Cases Today - ${data.deltarecovered} 😍 /n New Death Tolls Today - ${data.deltadeaths} 😭 
-
-    `);
+    ctx.reply(
+        `Country- IndiaTotal Cases- ${data.active} 😵`/n `Confirmed Cases- ${data.confirmed} 😢` /n `Deaths- ${data.deaths} 😭 `/n `New Cases Today- ${data.deltaconfirmed} 😱` /n `New Recovered Cases Today - ${data.deltarecovered} 😍` /n `New Death Tolls Today - ${data.deltadeaths} 😭 `);
 })
 
 // https://sumit735-bot.herokuapp.com/
